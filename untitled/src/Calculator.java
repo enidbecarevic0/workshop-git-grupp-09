de84fe2208a533d5550a3b6cc3968635a89016b9
oscarlavesson0 <oscarlavesson0@gmail.com>
2026-03-25 15:35:32 +0100

    DOcs: add Javadocs for sub()

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator{
     /** Returns the sum of a and b. */
     public static double add(double a, double b) { return a + b; }
-
+    /** Returns the difference of a and b. */
     public static double sub(double a, double b){return a - b; }
 
     public static double mul(double a, double b){return a * b; }
