bfe29ec981941a551631b5bc3b620c31948925d3
oscarlavesson0 <oscarlavesson0@gmail.com>
2026-03-25 15:42:59 +0100

    Feat: add main() entrypoint

--- untitled/src/App.java
+++ untitled/src/App.java
@@ -16,5 +16,7 @@ public class App{
         if ("4".equals(choice) && b == 0) System.out.println("Error: division by zero");
         if ("4".equals(choice) && b != 0) System.out.println(Calculator.div(a, b));
     }
-
+public static void main(String[] args) {
+        start();
+}
 }
