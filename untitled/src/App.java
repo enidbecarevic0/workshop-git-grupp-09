f3c699c4d84a035a511213c924591b135a5baddb
Joel <joelaxel.olsson@gmail.com>
2026-03-25 15:05:18 +0100

    Read operand a

--- untitled/src/App.java
+++ untitled/src/App.java
@@ -6,7 +6,7 @@ public class App{
         System.out.println("Your Choice: ");
         String choice = new java.util.Scanner(System.in).nextLine();
         if ("1".equals(choice)) System.out.println("TODO Add");
-        
+        double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
     }
 
 }
