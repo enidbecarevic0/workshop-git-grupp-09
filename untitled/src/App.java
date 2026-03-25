7c26db6d3110d69b21082ebd4b531f91abab9c0c
oscarlavesson0 <oscarlavesson0@gmail.com>
2026-03-25 16:12:34 +0100

    UI: add expliciti prompts for operands

--- untitled/src/App.java
+++ untitled/src/App.java
@@ -9,7 +9,9 @@ public class App{
         System.out.println("Your Choice: ");
         String option = new java.util.Scanner(System.in).nextLine();
         if ("1".equals(choice)) System.out.println("TODO Add");
+        System.out.println("A: ");
         double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
+        System.out.println("B: ");
         double b = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         System.out.println(Calculator.add(a,b));
         if ("2".equals(option)) System.out.println(Calculator.sub(a,b));
