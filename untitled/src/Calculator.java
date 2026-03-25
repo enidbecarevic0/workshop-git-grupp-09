3684eb0f398433e71991db28ef45c076603af352
oscarlavesson0 <oscarlavesson0@gmail.com>
2026-03-25 14:49:59 +0100

    Add Calculator.mul(a.b)

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -2,4 +2,6 @@ public class Calculator{
     public static double add(double a, double b) { return a + b; }
 
     public static double sub(double a, double b){return a - b; }
+
+    public static double mul(double a, double b){return a * b; }
 }
