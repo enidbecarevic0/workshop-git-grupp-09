2e1bf2f0583bdaef015f5e347a24769c548fd1b7
Joel <joelaxel.olsson@gmail.com>
2026-03-25 14:35:28 +0100

    Menu: add 'Subtract' + choice prompt

--- untitled/src/App.java
+++ untitled/src/App.java
@@ -1,8 +1,10 @@
 public class App{
     static void run(){
 
-        System.out.println("1) add/n0) Exit")
-        System.out.println("Choice: ");
+    
+        System.out.println("1) Subtract\n0) Exit");
+        System.out.println("2) Add \n0) Exit");
+        System.out.println("Your Choice: ");
         String choice = new java.util.Scanner(System.in).nextLine();
     }
 }
