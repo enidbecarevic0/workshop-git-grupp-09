8d772a433408b2d16dcb131dc2104e8c933012d0
StefanRaj <stefanraj@hotmail.se>
2026-03-25 15:33:50 +0100

    Docs: add javadoc for add()

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -1,4 +1,5 @@
 public class Calculator{
+    /** Returns the sum of a and b. */
     public static double add(double a, double b) { return a + b; }
 
     public static double sub(double a, double b){return a - b; }
