457b73913c91c0745c5f9e74ce43654c034abacc
Joel <joelaxel.olsson@gmail.com>
2026-03-25 15:37:24 +0100

    Docs: add Javadoc for mul()

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator{
     public static double add(double a, double b) { return a + b; }
     /** Returns the difference of a and b. */
     public static double sub(double a, double b){return a - b; }
-
+    /** Returns the product of a and b. */
     public static double mul(double a, double b){return a * b; }
 
     public static double div(double a, double b){return a / b; }
