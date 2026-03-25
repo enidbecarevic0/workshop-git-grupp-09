7ac3a452503527c6be316ed744f801f93d52f79c
StefanRaj <stefanraj@hotmail.se>
2026-03-25 16:06:08 +0100

    Test: add sub() test

--- untitled/src/TestCalculator.java
+++ untitled/src/TestCalculator.java
@@ -1,5 +1,6 @@
 public class TestCalculator {
     public static void main(String[] args) {
         if(Calculator.add(2, 3) !=5) throw new AssertionError();
+        if(Calculator.sub(5, 2) != 3) throw new AssertionError();
     }
 }
