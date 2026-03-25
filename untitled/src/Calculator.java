8b98e908581feb327c98c3a87ce6b401bbeebd47
EmilHadzic123 <hadzic.emil@yahoo.se>
2026-03-25 15:39:22 +0100

    Docs: adda Javadoc for dic()

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -5,6 +5,6 @@ public class Calculator{
     public static double sub(double a, double b){return a - b; }
     /** Returns the product of a and b. */
     public static double mul(double a, double b){return a * b; }
-
+    /** Returns the quotient of a and b. */
     public static double div(double a, double b){return a / b; }
 }
