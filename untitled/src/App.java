1d673d5d1d326c6b98bbd9a96ed97359a01fc2d0
oscarlavesson0 <oscarlavesson0@gmail.com>
2026-03-25 16:08:35 +0100

    Refactor: compare option for choice 2

--- untitled/src/App.java
+++ untitled/src/App.java
@@ -12,7 +12,7 @@ public class App{
         double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         double b = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         System.out.println(Calculator.add(a,b));
-        if ("2".equals(choice)) System.out.println(Calculator.sub(a,b));
+        if ("2".equals(option)) System.out.println(Calculator.sub(a,b));
         if ("3".equals(choice)) System.out.println(Calculator.mul(a, b));
         if ("4".equals(choice) && b == 0) System.out.println("Error: division by zero");
         if ("4".equals(choice) && b != 0) System.out.println(Calculator.div(a, b));
